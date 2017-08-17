aa99879131cbe3e3815dbcfcbcdfde4285b53584
Wojciech Gac <wojciech.s.gac@gmail.com>
2017-08-17 09:41:50 +0200

    Put GPLv3 copyright notices in files

--- cl-moss.lisp
+++ cl-moss.lisp
@@ -1,4 +1,22 @@
 ;;;; cl-moss.lisp
+;;;;
+;;;; Copyright (C) 2017 Wojciech Gac <wojciech.s.gac@gmail.com>
+;;;;
+;;;; This file is part of CL-MOSS.
+;;;;
+;;;; CL-MOSS is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; CL-MOSS is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with CL-MOSS.  If not, see <http://www.gnu.org/licenses/>.
+;;;;
 
 (in-package #:cl-moss)
 
@@ -69,7 +87,7 @@
              limit)))
 
 (defmethod set-comment-string ((moss moss) comment)
-  (rplacd (assoc "c" (moss-options moss) :test #'equalp) 
+  (rplacd (assoc "c" (moss-options moss) :test #'equalp)
           (princ-to-string comment)))
 
 (defmethod set-result-limit ((moss moss) limit)
